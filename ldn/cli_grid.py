5da9b5f3059df11361a0399390c98d4a2962924d
willjnz <will.jones@hotmail.co.nz>
2026-01-20 12:15:52 +1100

    Use country codes as well as names

--- ldn/cli_grid.py
+++ ldn/cli_grid.py
@@ -4,131 +4,102 @@ import typer
 
 cli_grid_app = typer.Typer()
 
-# https://en.wikipedia.org/wiki/Small_Island_Developing_States
-SIDS_COUNTRIES = [
+SIDS_COUNTRIES_AND_CODES = {
     # Caribbean
-    "Anguilla",
-    "Antigua and Barbuda",
-    "Aruba",
-    "Bahamas",
-    "Barbados",
-    "Belize",
-    "Bermuda",
-    "British Virgin Islands",
-    "Cayman Islands",
-    "Cuba",
-    "Curaçao",
-    "Dominica",
-    "Dominican Republic",
-    "Grenada",
-    "Guadeloupe",
-    "Guyana",
-    "Haiti",
-    "Jamaica",
-    "Martinique",
-    "Montserrat",
-    "Puerto Rico",
-    "Saint Kitts and Nevis",
-    "Saint Lucia",
-    "Saint Vincent and the Grenadines",
-    "Sint Maarten",
-    "Suriname",
-    "Trinidad and Tobago",
-    "Turks and Caicos Islands",
-    "U.S. Virgin Islands",
+    "Anguilla": "AIA",
+    "Antigua and Barbuda": "ATG",
+    "Aruba": "ABW",
+    "Bahamas": "BHS",
+    "Barbados": "BRB",
+    "Belize": "BLZ",
+    "Bermuda": "BMU",
+    "British Virgin Islands": "VGB",
+    "Cayman Islands": "CYM",
+    "Cuba": "CUB",
+    "Curaçao": "CUW",
+    "Dominica": "DMA",
+    "Dominican Republic": "DOM",
+    "Grenada": "GRD",
+    "Guadeloupe": "GLP",
+    "Guyana": "GUY",
+    "Haiti": "HTI",
+    "Jamaica": "JAM",
+    "Martinique": "MTQ",
+    "Montserrat": "MSR",
+    "Puerto Rico": "PRI",
+    "Saint Kitts and Nevis": "KNA",
+    "Saint Lucia": "LCA",
+    "Saint Vincent and the Grenadines": "VCT",
+    "Sint Maarten": "SXM",
+    "Suriname": "SUR",
+    "Trinidad and Tobago": "TTO",
+    "Turks and Caicos Islands": "TCA",
+    "U.S. Virgin Islands": "VIR",
     # Pacific
-    "American Samoa",
-    "Cook Islands",
-    "Fiji",
-    "French Polynesia",
-    "Guam",
-    "Kiribati",
-    "Marshall Islands",
-    "Micronesia",
-    "Nauru",
-    "New Caledonia",
-    "Niue",
-    "Northern Mariana Islands",
-    "Palau",
-    "Papua New Guinea",
-    "Samoa",
-    "Solomon Islands",
-    "Timor-Leste",
-    "Tonga",
-    "Tuvalu",
-    "Vanuatu",
-
+    "American Samoa": "ASM",
+    "Cook Islands": "COK",
+    "Fiji": "FJI",
+    "French Polynesia": "PYF",
+    "Guam": "GUM",
+    "Kiribati": "KIR",
+    "Marshall Islands": "MHL",
+    "Micronesia": "FSM",
+    "Nauru": "NRU",
+    "New Caledonia": "NCL",
+    "Niue": "NIU",
+    "Northern Mariana Islands": "MNP",
+    "Palau": "PLW",
+    "Papua New Guinea": "PNG",
+    "Samoa": "WSM",
+    "Solomon Islands": "SLB",
+    "Timor-Leste": "TLS",
+    "Tonga": "TON",
+    "Tuvalu": "TUV",
+    "Vanuatu": "VUT",
     # Africa, Indian Ocean, Mediterranean, South China Sea (AIMS)
-    "Cape Verde",
-    "Comoros",
-    "Guinea-Bissau",
-    "Maldives",
-    "Mauritius",
-    "São Tomé and Príncipe",
-    "Seychelles",
-    "Singapore",	
-]
+    "Cape Verde": "CPV",
+    "Comoros": "COM",
+    "Guinea-Bissau": "GNB",
+    "Maldives": "MDV",
+    "Mauritius": "MUS",
+    "São Tomé and Príncipe": "STP",
+    "Seychelles": "SYC",
+    "Singapore": "SGP",
+}
 # https://github.com/digitalearthpacific/dep-tools/blob/main/dep_tools/grids.py#L23
-DEP_COUNTRIES = [
-    "American Samoa",
-    "Cook Islands",
-    "Fiji",
-    "French Polynesia",
-    "Guam",
-    "Kiribati",
-    "Marshall Islands",
-    "Micronesia",
-    "Nauru",
-    "New Caledonia",
-    "Niue",
-    "Northern Mariana Islands",
-    "Palau",
-    "Papua New Guinea",
-    "Pitcairn Islands",
-    "Solomon Islands",
-    "Samoa",
-    "Tokelau",
-    "Tonga",
-    "Tuvalu",
-    "Vanuatu",
-    "Wallis and Futuna",
-]
-# TODO: Maybe we need the country codes as well as the names.
-# DEP_COUNTRIES_AND_CODES = {
-#     "American Samoa": "ASM",
-#     "Cook Islands": "COK",
-#     "Fiji": "FJI",
-#     "French Polynesia": "PYF",
-#     "Guam": "GUM",
-#     "Kiribati": "KIR",
-#     "Marshall Islands": "MHL",
-#     "Micronesia": "FSM",
-#     "Nauru": "NRU",
-#     "New Caledonia": "NCL",
-#     "Niue": "NIU",
-#     "Northern Mariana Islands": "MNP",
-#     "Palau": "PLW",
-#     "Papua New Guinea": "PNG",
-#     "Pitcairn Islands": "PCN",
-#     "Solomon Islands": "SLB",
-#     "Samoa": "WSM",
-#     "Tokelau": "TKL",
-#     "Tonga": "TON",
-#     "Tuvalu": "TUV",
-#     "Vanuatu": "VUT",
-#     "Wallis and Futuna": "WLF",
-# }
+DEP_COUNTRIES_AND_CODES = {
+    "American Samoa": "ASM",
+    "Cook Islands": "COK",
+    "Fiji": "FJI",
+    "French Polynesia": "PYF",
+    "Guam": "GUM",
+    "Kiribati": "KIR",
+    "Marshall Islands": "MHL",
+    "Micronesia": "FSM",
+    "Nauru": "NRU",
+    "New Caledonia": "NCL",
+    "Niue": "NIU",
+    "Northern Mariana Islands": "MNP",
+    "Palau": "PLW",
+    "Papua New Guinea": "PNG",
+    "Pitcairn Islands": "PCN",
+    "Solomon Islands": "SLB",
+    "Samoa": "WSM",
+    "Tokelau": "TKL",
+    "Tonga": "TON",
+    "Tuvalu": "TUV",
+    "Vanuatu": "VUT",
+    "Wallis and Futuna": "WLF",
+}
 
 @cli_grid_app.command("list-countries")
-def list_countries() -> list[str]:
-    logging.info("Listing all Small Island Developing States (SIDS) and Digital Earth Pacific (DEP) countries.")
-    count_deps = len(DEP_COUNTRIES)
-    count_sids = len(SIDS_COUNTRIES)
-    logging.info(f"Number of SIDS countries: {count_sids}")
-    logging.info(f"Number of DEP countries: {count_deps}")
-    countries_set = set(SIDS_COUNTRIES + DEP_COUNTRIES)
-    countries_list = list(countries_set)
-    countries_list.sort()
-    logging.info(f"Total combined countries: {len(countries_list)}, ({count_sids + count_deps} before deduplication)")
-    logging.info(f"Countries list {countries_list}")
-    return countries_list
+def list_countries() -> dict:
+    """List all unique SIDS and DEP countries as a dict of name: code."""
+    logging.info("Listing all 'Small Island Developing States (SIDS)' and 'Digital Earth Pacific (DEP)' countries.")
+    # Merge dicts, DEP override SIDS if duplicate name
+    combined = {**SIDS_COUNTRIES_AND_CODES, **DEP_COUNTRIES_AND_CODES}
+    # TODO: There are no country code duplicates/conflicts in this data, but if expanding the use of this function, check for country code conflicts too.
+    sorted_combined = dict(sorted(combined.items())) # Sort by country name
+    logging.info(f"Total unique countries from both datasets: {len(sorted_combined)}")
+    logging.info(sorted_combined)
+    return sorted_combined
