0fd11d1cbd8cd3a7b3adc85a808d4588bece24ee
willjnz <will.jones@hotmail.co.nz>
2026-02-20 12:01:07 +1100

    Use consistent CRS variable. Align products by resampling. Make agreeing product class layer. Fix import of typology_mapping.yaml

--- ldn/utils.py
+++ ldn/utils.py
@@ -1,4 +1,5 @@
 import yaml
+from pathlib import Path
 from dep_tools.grids import COUNTRIES_AND_CODES as DEP_COUNTRIES_AND_CODES
 
 SIDS_COUNTRIES_AND_CODES = {
@@ -74,5 +75,6 @@ NON_DEP_COUNTRIES = {
     if k not in DEP_COUNTRIES_AND_CODES
 }
 
-with open("typology_mapping.yaml") as f:
+_TYP_FILE = Path(__file__).resolve().parent / "typology_mapping.yaml"
+with _TYP_FILE.open("r", encoding="utf-8") as f:
     typology_mapping = yaml.safe_load(f)
