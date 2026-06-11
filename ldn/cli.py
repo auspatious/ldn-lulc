e7aee190a7839353c042dcae8ffd741d294c15ff
willjnz <will.jones@hotmail.co.nz>
2026-06-11 11:12:41 +1000

    Don't do it twice

--- ldn/cli.py
+++ ldn/cli.py
@@ -102,6 +102,8 @@ def _find_existing_tasks(
 
     sc_prefix = _source_coop_prefix(dataset_id)
 
+    sc_prefix = _source_coop_prefix(dataset_id)
+
     # Collect unique (bucket, owner) combos
     region_combos: set[tuple[str, str]] = set()
     for task in tasks:
